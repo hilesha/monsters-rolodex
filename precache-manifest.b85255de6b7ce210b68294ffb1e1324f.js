66aab256977a35a63b3b33713faa9e4b668f89bb
Aswani <hilesha@BM6283.local>
2019-09-29 04:06:00 +0530

    Updates

---
+++ precache-manifest.b85255de6b7ce210b68294ffb1e1324f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17c1f3bc9409ad3fefe8c6083dc335e6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "89f0373674d107759338",
+    "url": "/monsters-rolodex/static/css/main.5f229626.chunk.css"
+  },
+  {
+    "revision": "877ab33c4cabc00823c7",
+    "url": "/monsters-rolodex/static/js/2.4586650f.chunk.js"
+  },
+  {
+    "revision": "89f0373674d107759338",
+    "url": "/monsters-rolodex/static/js/main.047bee73.chunk.js"
+  },
+  {
+    "revision": "29b8639263678424eef0",
+    "url": "/monsters-rolodex/static/js/runtime-main.7352cc78.js"
+  }
+]);
